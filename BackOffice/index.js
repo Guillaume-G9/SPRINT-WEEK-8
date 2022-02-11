8242e468aee2f4fcd574932ccd99833d6c1eab53
Derma6 <maxime.1109@hotmail.fr>
2022-02-11 16:56:54 +0100

    add article

--- BackOffice/index.js
+++ BackOffice/index.js
@@ -1,3 +1,36 @@
-const addPage = document.getElementById('addPage')
-const addArticle = document.getElementById('addArticle')
+const addPageBtn = document.querySelector('#addPage')
+const addArticleBtn = document.querySelector('#addArticle')
+
+const article = document.querySelector('article')
+
+addArticleBtn.addEventListener('click', () => {
+    article.insertAdjacentElement('afterend', article.cloneNode(true))
+})    
+
+const h1 = document.querySelector('h1')
+
+function saveContent(title, contenu) {
+    localStorage.setItem(title, contenu)
+}
+
+function loadContent(title) {
+    return localStorage.getItem(title)
+}
+
+addPageBtn.addEventListener('click', () => {
+    h1.innerHTML = "Menu"
+    saveContent("h1", "Menu")
+})
+
+
+const chargement = (title) => {
+    if (!localStorage.getItem(title)) {
+   h1.innerHTML = "Arrays"
+} else {
+    h1.innerHTML = loadContent('h1')
+}}
+
+window.onload = chargement('h1')
+
+
 
