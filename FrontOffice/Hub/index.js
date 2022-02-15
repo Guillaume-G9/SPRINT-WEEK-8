ba93266c27c5de81af801212c8b90def38cba650
Derma6 <maxime.1109@hotmail.fr>
2022-02-15 11:44:47 +0100

    add filter

--- FrontOffice/Hub/index.js
+++ FrontOffice/Hub/index.js
@@ -1,8 +1,13 @@
-let article = document.querySelector('ul');
+const article = document.querySelector('ul');
+const searchBar = document.querySelector('input');
+let dataAPI = [];
 
 fetch('https://strapi-gogokodo.herokuapp.com/api/sources')
     .then(response => response.json())
-    .then(data => display(data.data))
+    .then(data => { 
+        display(data.data)
+        dataAPI = data.data
+    })
 
 function display (data) {
     // for (video of data) {
@@ -12,14 +17,24 @@ function display (data) {
     //         <a href="${video.attributes.url}" target = "_blank" >Visiter</a>
     //     </li>`
     // }
-    // console.log(data)
+    article.innerHTML = "";
 
     data.map(video => {
         article.innerHTML += 
         `<li>
             <h3>${video.attributes.title}</h3>
-            <a href="${video.attributes.url}" target = "_blank" >Visiter</a>
+            <a href="${video.attributes.url}" target="_blank">Visiter</a>
         </li>`
     })
 }
 
+searchBar.addEventListener('keyup', () => {
+    let input = searchBar.value
+    input=input.toLowerCase();
+    const filter = dataAPI.filter(dataAPI => {
+        return (
+        dataAPI.attributes.category.toLowerCase().includes(input) || dataAPI.attributes.title.toLowerCase().includes(input)
+        )
+    })
+    display(filter)
+})
